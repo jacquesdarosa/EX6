bb2f8c2abef2a7d312b6e804ccbbedff6851730a
Jacques <jacquesdarosa@gmail.com>
2022-08-17 14:01:21 +0200

    EX6

--- exercise.py
+++ exercise.py
@@ -6,7 +6,7 @@
 # task 1
 
 
-'''!three_mul = 'fizz'
+three_mul = 'fizz'
 five_mul = 'buzz'
 num1 = 3
 num2 = 5
@@ -19,11 +19,11 @@ for i in range(1, max_num):
 
     elif i%num2 == 0:
         print(i, five_mul)
-    elif i%num1 == 0:'
-        print(i, three_mul)'''
+    elif i%num1 == 0:
+     print(i, three_mul)
 
 
-'''# task 2 
+# task 2 
 # sum: 1 to 5 # I changed the variable name from Sum to Total
 n = 5
 number = 1
@@ -31,77 +31,86 @@ total = 0
 while number < n + 1 :
     total += number
     number += + 1
-print('Total =', total)'''
+print('Total =', total)
 
 
 # task 3
-'''n = 5
+n = 5
 sum = 0
 
-for num in range(n): 
+for num in range(n+1): 
     sum += num
-print("Sum=", sum)'''
+print("Sum=", sum)
 
 # task 4
+ # 4.1
+for x in range(3):
+        print(x)
 
-'''for j in range(5):
-     print("This is loop number", j)'''
-
-'''countdown = 5
+# 4.2
+for j in range(5):
+     print("This is loop number", j)
+# 4.3
+x = 10    
+while x > 0:
+    print(x)
+    x -= 1
+# 4.4
+countdown = 5
 while countdown >= 0 :
     if countdown !=0 :
       print(countdown)
     countdown -= 1
 else: 
-    print("Start!")'''
+    print("Start!")
 
 # task 5
 
-'''x = int(input("First number: "))
+x = int(input("First number: "))
 y = int(input("Second number: "))
 z = int(input("Third number: "))
 
 if x == y or y == z or z== x:
-   sum = 0 
+   result = 0 
 else:
     sum = x + y + z
-print("Calculated sum is ", sum)'''
+print("Calculated sum is ", result)
 
 # task 6
 
-'''x = int(input("First number: "))
+x = int(input("First number: "))
 y = int(input("Second number: "))
 
 result = x + y
 
 if result > 15 and result < 20:
     result = 20
-print("Calculated sum is ", result)'''
+print("Calculated sum is ", result)
 
 
 # task 7
 
-'''a = input("First value: ")
+a = input("First value: ")
 b = input("Second value: ")
 
 print("Before swapping: a =", a, " , b=", b)
 
-print("After swapping: a =", b, " , b=", a)'''
+print("After swapping: a =", b, " , b=", a)
 
 # task 8
 
-'''x = float(input("First number: "))
+x = float(input("First number: "))
 y = float(input("Second number: "))
 z = float(input("Third number: "))
 
 print("The maximum value is ", max(x, y, z))
-print("The minimum value is ", min(x, y, z))'''
+print("The minimum value is ", min(x, y, z))
 
 
 # task 9
 
 
-'''x = input("Type your value:")
+x = input("Type your value:")
 
 if x == "0":
     x = False
@@ -110,9 +119,9 @@ elif x == "1":
 else:
   pass
 
-print("Your entered value is now ", x)'''
+print("Your entered value is now ", x)
 
-'''# 10
+# 10
 x = int(input("First number:"))
 y = int(input("Second number:"))
 
@@ -121,4 +130,4 @@ if x % y == 0:
 elif y % x == 0:
     print("Second number is divisible by first number, result =", y / x)
 else:
-    print("Numbers are non-divisible!")'''
+    print("Numbers are non-divisible!")
