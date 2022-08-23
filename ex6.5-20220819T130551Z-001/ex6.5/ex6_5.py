752b7bf411845e07fde76dc56e94a3547baeaa36
Jacques <jacquesdarosa@gmail.com>
2022-08-23 11:01:26 +0200

    new_changed3

--- ex6.5-20220819T130551Z-001/ex6.5/ex6_5.py
+++ ex6.5-20220819T130551Z-001/ex6.5/ex6_5.py
@@ -7,7 +7,7 @@
 # this is my story 
 
 print("You're in a boat all alone in the middle of the ocean with only 500 ml of drinking water left. What do you do?")
-print("(1) drink all the water (2) drink some (3) you do nothing")
+print("(1) you drink all the water (2) you drink some (3) you do nothing")
 choice = input("choose: ")
 
 if choice == "1":
@@ -18,5 +18,5 @@ elif choice == "2":
 elif choice == "3": 
    print("only God knows what will happen to you")
 else:
-   print("u just die from inaction")
+   print("you're rescued by the Coast Guard")
 
