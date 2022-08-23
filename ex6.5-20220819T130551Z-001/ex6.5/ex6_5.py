930f58a436de6452a50637605e7d996ca4a5c502
Jacques <jacquesdarosa@gmail.com>
2022-08-23 22:52:28 +0200

    last_change

--- ex6.5-20220819T130551Z-001/ex6.5/ex6_5.py
+++ ex6.5-20220819T130551Z-001/ex6.5/ex6_5.py
@@ -11,18 +11,40 @@ print("(1) you drink all the water (2) you drink some (3) you do nothing")
 choice = input("choose: ")
 
 if choice == "1":
+
    print("your chances of surviving this are not high")
-   choice = input('Would you like to think again? Please type yes/no:')
-   if choice == "yes" :
-    print('you can live a bit longer if you do not drink all the water in one go')
-   elif choice == "no":
-    print('You will probably die soon, so Carpe Diem')
+
+   choice_2 =input('Would you like to think again? Please type yes/no:')
+
+   if  choice_2 == "yes":
+          print('you can live a bit longer if you do not drink all the water in one go')
+   
+   elif  choice_2 == "no":
+     print('You will probably die soon, so Carpe Diem')
+
+
+
+
 
 elif choice == "2":
-   print("good choice, but you should drink very little, try to save some water if you wanna survive")
+    print("good choice, but you should drink very little, try to save some water if you wanna survive")
+    print(int(input('How much do you want to drink? Please enter number in ml:')))
    #continue tree here
-elif choice == "3": 
-   print("only God knows what will happen to you")
-else:
-   print("you're rescued by the Coast Guard, life is good")
+    for ml in (1, 500):
 
+      if 50 < ml < 100: 
+       print('You might live roughly up to 24 hours')
+      elif 100 < ml <=150:
+       print('be careful, you will probably run out of water in less than 24 hours')
+      elif 150 < ml <=250: 
+       print(' you might live perhaps more than 15 hours')
+      elif 250 < ml <= 300: 
+       print('This is not good, man, this is going too fast', "you've got 10 hours maybe?")
+      elif 300 < ml: 
+       print('Really, do you want to die here?:')
+   
+if choice == "3" :
+   print("only God knows what will happen to you")
+
+else: print("you're rescued by the Coast Guard, life is good")
+   
